ab35b5584942ff901ea9b5eb27cd50fb4100991c
Liene31 <67922137+Liene31@users.noreply.github.com>
2025-06-03 14:17:11 +0200

    Finalised JS

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
+const cards = [
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "JACK",
+  "QUEEN",
+  "KING",
+  "ACE",
+];
+const gameStatusEl = document.getElementById("game-status");
 let cardArray = [];
 let deckId = "";
 let remainingCards = "";
+let computerScore = 0;
+let yourScore = 0;
 
 document.getElementById("new-deck-btn").addEventListener("click", () => {
   fetch("https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
       deckId = data.deck_id;
       remainingCards = data.remaining;
       updateRemainingCards();
@@ -17,11 +34,21 @@ document.getElementById("draw-btn").addEventListener("click", () => {
   fetch(`https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
       remainingCards = data.remaining;
       cardArray = data.cards;
       updateRemainingCards();
       displayCards();
+      displayScore(data.cards[0].value, data.cards[1].value);
+      if (remainingCards === 0) {
+        document.getElementById("draw-btn").disabled = true;
+        if (computerScore > yourScore) {
+          gameStatusEl.textContent = "Computer won 😞";
+        } else if (yourScore > computerScore) {
+          gameStatusEl.textContent = "You won 🥳";
+        } else {
+          gameStatusEl.textContent = "it's a tie 🤝";
+        }
+      }
     });
 });
 
@@ -38,6 +65,26 @@ function displayCards() {
     <div class="card"><img src="${card.image}"></div>
     `;
   });
-
   document.getElementById("card-container").innerHTML = displayCardsHtml;
+}
+
+function displayScore(card1, card2) {
+  const cardOneValue = cards.indexOf(card1);
+  const cardTwoValue = cards.indexOf(card2);
+
+  if (cardOneValue > cardTwoValue) {
+    computerScore++;
+    gameStatusEl.textContent = "Computer won this turn";
+  } else if (cardTwoValue > cardOneValue) {
+    yourScore++;
+    gameStatusEl.textContent = "You won this turn";
+  } else {
+    gameStatusEl.textContent = "It's a war!";
+  }
+  document.getElementById(
+    "computer-score"
+  ).textContent = `Compure score: ${computerScore}`;
+  document.getElementById(
+    "your-score"
+  ).textContent = `Your score: ${yourScore}`;
 }
